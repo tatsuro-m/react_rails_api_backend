99e256a2f5bec46694d20acba11f82853e6d2530
Tatsuro Miyamoto <mtatsu0130@gmail.com>
2020-10-11 07:17:47 +0900

    Post モデルの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |n|
+Post.create(
+    title: "サンプル#{n}",
+    content: "これはseedで作成したものです。"
+)
+end
